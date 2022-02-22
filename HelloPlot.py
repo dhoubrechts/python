6a11080fcdfd2dde4d43006978084e0a3b673751
dhoubrechts <dominique.houbrechts@gmail.com>
2022-02-22 14:33:16 +0100

    test

--- HelloPlot.py
+++ HelloPlot.py
@@ -1,4 +1,19 @@
 # This program prints Hello, world!
 
 
-print('Hello Plot 13')
+ 
+
+print('Hello Plot 14')
+
+
+def showMenu():
+    print('Main Menu')
+    print('1. Play game')
+    print('2. View high scores')
+    print('3. Quit')
+
+showMenu()
+
+
+print ("Dit is leuk")
+
