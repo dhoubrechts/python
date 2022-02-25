1779736d01a6a6655eab21f87fb614553e49dadd
Dominique Houbrechts <dominique.houbrechts@gmail.com>
2022-02-25 11:52:37 +0100

    test

--- HelloPlot.py
+++ HelloPlot.py
@@ -7,13 +7,21 @@ print('Hello Plot 14')
 
 
 def showMenu():
+    age=20
+    
     print('Main Menu')
     print('1. Play game')
     print('2. View high scores')
     print('3. Quit')
+    if (age < 10):
+        print("hallo zeg")
+    else:
+       print("niet dan")
+
 
 showMenu()
 
 
-print ("Dit is leuk")
-
+age=65
+pi=3.14159
+print (f"Je bent {age} jaar oud, en kent het getal {pi}")
